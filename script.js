d32f5c8821bce5377a693bd38fdfbd39bfc9b877
SOUMEN-PAL <112452467+SOUMEN-PAL@users.noreply.github.com>
2022-11-13 12:59:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var names = new Array();
+names[0]="takov"
+names[1]="Jakov"
+names[2]="jen"
+names[3]="paul"
+names[4]="jerry"
+names[5]="tome"
+names[6]="Larry"
+names[7]="Jason"
+names[8]="SID"
+names[9]="jimmy"
+
+
+for (var i=0;i<names.length;i++){
+    if(names[i].charAt(0)==="j" || names[i].charAt(0)=== "j"){
+        console.log("Goodbye "+names[i])
+    }
+    else{
+        console.log("Hello "+names[i])
+    }
+}
+
+
+
+
